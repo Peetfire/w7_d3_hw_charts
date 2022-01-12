c3e7c741668b40f83ecc594df0f27b9ab55a70d6
petermordaunt <peetfire@hotmail.com>
2022-01-12 16:54:50 +0000

    functionality - songs fetched and set to state

--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ChartList from "../components/ChartList";
 
 const ChartContainer = () => {
+
+    const[songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        getSongs()
+    })
+
+    const getSongs = () => {
+        fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/json")
+        .then(response => response.json())
+        .then(songs => setSongs(songs));
+    }
+
     return ( 
         <div className="chart-container">
             <h1>I am Charticus...container</h1>
-            <ChartList/>
+            <ChartList songs={songs}/>
         </div>
     )
 }
