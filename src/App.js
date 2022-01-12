b2d6217ec122e8c1b04eabc9b548409e3ff22109
petermordaunt <peetfire@hotmail.com>
2022-01-12 15:43:14 +0000

    set up - static ChartContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ChartContainer from './containers/ChartContainer';
 
 function App() {
   return (
     <div>
       <h1>Hiya World!</h1>
+      <ChartContainer/>
     </div>
   )
 }
---
+++ src/containers/ChartContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChartContainer = () => {
+    return ( 
+        <div className="chart-container">
+            <h1>I am Charticus...container</h1>
+        </div>
+    )
+}
+
+export default ChartContainer;
