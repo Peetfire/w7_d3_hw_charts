fb25f7081ed90c4fae45ce9a925f9d5a1d656af3
petermordaunt <peetfire@hotmail.com>
2022-01-12 15:47:14 +0000

    setup - static ChartList

---
+++ src/components/ChartList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChartList = () => {
+    return (
+        <div className="class-list">
+            <ul>
+                I am UList
+            </ul>
+        </div>
+    )
+}
+
+export default ChartList;
--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ChartList from "../components/ChartList";
 
 const ChartContainer = () => {
     return ( 
         <div className="chart-container">
             <h1>I am Charticus...container</h1>
+            <ChartList/>
         </div>
     )
 }
